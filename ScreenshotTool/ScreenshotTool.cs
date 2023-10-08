fc45063bddccdbb92abd1374d22a6c5a6516d7a9
jonas drach <drach.jonas@web.de>
2023-10-08 14:25:33 +0200

    add style program loading

--- ScreenshotTool/ScreenshotTool.cs
+++ ScreenshotTool/ScreenshotTool.cs
@@ -10,6 +10,7 @@ namespace ScreenTool
         public static string Imgure_ClientID = "";
         public static string Imgure_Token = "";
         public static bool AutoUpload = false;
+        public static int StyleIndex = 0;
         public static ConfigAPI ConfigAPI { get; set; }
         public static ConfigModel config;
 
@@ -24,6 +25,7 @@ namespace ScreenTool
             ConfigAPI = new ConfigAPI();
             config = ConfigAPI.Initialize();
             AutoUpload = config.AutoUpload;
+            StyleIndex = config.StyleIndex;
 
             ApplicationConfiguration.Initialize();
             Application.Run(new Views.Window());
