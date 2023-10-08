3fe1cf248cb89bbb9061090bf816de917ce6cfb3
jonas drach <drach.jonas@web.de>
2023-10-08 13:29:20 +0200

    add config init

--- ScreenshotTool/Program.cs
+++ ScreenshotTool/Program.cs
@@ -1,10 +1,16 @@
+using ScreenshotTool.API;
+using System.Configuration;
+using static ScreenshotTool.API.ConfigAPI;
+
 namespace ScreenTool
 {
     internal static class Program
     {
-        public readonly static string ProgramPath = @"C:\Mongo";
-        public readonly static string Imgure_ClientID = "";
-        public readonly static string Imgure_Token = "";
+        public static string ProgramPath = @"C:\Mongo";
+        public static string Imgure_ClientID = "";
+        public static string Imgure_Token = "";
+        public static ConfigAPI ConfigAPI { get; set; }
+        public static ConfigModel config;
 
         [STAThread]
         static void Main()
@@ -13,6 +19,10 @@ namespace ScreenTool
             {
                 Directory.CreateDirectory(ProgramPath);
             }
+
+            ConfigAPI = new ConfigAPI();
+            config = ConfigAPI.Initialize();
+
             ApplicationConfiguration.Initialize();
             Application.Run(new Views.Window());
         }
